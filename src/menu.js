f2cebe2c60b561581776b389b93eb8ae2c5fb2b9
haraphat01 <haraphat@gmail.com>
2020-10-08 14:41:51 +0100

    Working on the menu listinings

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,28 @@
-import Fud1 from './images/garri.jpeg';
-import Fud2 from './images/rice.jpeg';
-import Fud3 from './images/tuwo.jpeg';
-import Fud4 from './images/yam.jpeg';
+import Fud1 from './images/garri.jpg';
+import Fud2 from './images/rice.jpg';
+import Fud3 from './images/tuwo.jpg';
+import Fud4 from './images/yam.jpg';
 
 export const menu = () => {
     let element = document.createElement('div')
-    food = [fud1, fud2, fud3, fud4]
+    let food = [fud1, fud2, fud3, fud4]
+    
+    for (let i = 0; i < food.length; i += 1){
+     const listing = document.createElement('li');
+     listing.classList.add = ('listing');
+     let images = document.createElement('img')
+     images = food[i]
+     let title = document.createElement('p')
+     title.classList.add('title')
+     title.innerHTML = "Random Title"
+     let price = document.createElement('p')
+     price.classList.add('price')
+     price.innerHTML = (Math.random() * 100)
+     
+
+     listing.appendChild(images)
+     listing.appendChild(title)
+     listing.appendChild(price)
+    }
 }
 
