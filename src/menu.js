7d82a9009c8f0bfd32c90089d1be862aaf25e9b9
haraphat01 <haraphat@gmail.com>
2020-10-09 10:55:43 +0100

    Fixed the lint errors

--- src/menu.js
+++ src/menu.js
@@ -5,6 +5,7 @@ import fud4 from './images/yam.jpg';
 
 export const menu = () => {
     let element = document.createElement('div')
+    element.classList.add = ('iyawon')
     let food = [fud1, fud2, fud3, fud4]
     let p_menu = document.createElement('ul')
     p_menu.classList.add('p_menu')
@@ -12,14 +13,14 @@ export const menu = () => {
     let listing = document.createElement('li');
      listing.classList.add = ('listing');
      let images = document.createElement('img')
-     images.classList.add = ('images')
+     images.classList.add = ('image')
      images.src = food[i]
      let title = document.createElement('p')
      title.classList.add('title')
      title.innerHTML = "Random Title"
      let price = document.createElement('p')
      price.classList.add('price')
-     price.innerHTML = (Math.random() * 100)
+     price.innerHTML = '#'+ +(Math.ceil(Math.random() * 100)) + +".00"
      
 
      listing.append(images, title, price)
